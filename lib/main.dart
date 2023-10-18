44a6748217c0ed99e127ffc6888a7ae0220d9b71
Charles Xavier <charlesxsaldanha@hotmail.com>
2023-10-18 16:09:32 -0300

    feat: configurado main.
    criado e renderizado os cards com informações vindo da api. Criado renderizações para se der algo errado. Criado função de cores aleatorias para ser usado posteriormente

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,42 @@
+import 'package:awesome_icons/awesome_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:lista_contatos/http/http_client.dart';
+import 'package:lista_contatos/repositories/contatos_repository.dart';
+import 'package:lista_contatos/store/contatos_store.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
 }
 
+Color getRandomColor() {
+  final random = Random();
+  final alfa = random.nextInt(255);
+  final red = random.nextInt(256);
+  final green = random.nextInt(256);
+  final blue = random.nextInt(256);
+  return Color.fromARGB(alfa, red, green, blue);
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Lista de Contatos',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Contatos'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -55,71 +45,132 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final ContatoStore store = ContatoStore(
+    repository: ContatosRepository(
+      client: HttpClient(),
+    ),
+  );
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    store.getContatos();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: AnimatedBuilder(
+        animation: Listenable.merge([
+          store.isLoading,
+          store.erro,
+          store.state,
+        ]),
+        builder: (context, child) {
+          if (store.isLoading.value) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (store.erro.value.isNotEmpty) {
+            return const Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(FontAwesomeIcons.frown,
+                      color: Colors.indigoAccent, size: 40),
+                  Padding(padding: EdgeInsets.symmetric(vertical: 15)),
+                  Text(
+                    textAlign: TextAlign.center,
+                    'Não foi possível carregar a lista de contatos',
+                    style: TextStyle(
+                      color: Colors.indigoAccent,
+                      fontWeight: FontWeight.w700,
+                      fontSize: 20,
+                    ),
+                  ),
+                ],
+              ),
+            );
+          }
+
+          if (store.state.value!.contatos.isEmpty) {
+            return const Center(
+              child: Text(
+                'Nenhum contato salvo',
+                style: TextStyle(
+                  color: Colors.indigoAccent,
+                  fontWeight: FontWeight.w700,
+                  fontSize: 20,
+                ),
+              ),
+            );
+          } else {
+            return ListView.builder(
+              itemCount: store.state.value!.contatos.length,
+              itemBuilder: (context, index) {
+                final sortedContatos = store.state.value!.contatos;
+                sortedContatos.sort((a, b) {
+                  return (a.nome ?? '').compareTo(b.nome ?? '');
+                });
+                final item = sortedContatos[index];
+                final cores = item.cor;
+                return GestureDetector(
+                  onTap: () {},
+                  child: Card(
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 8, vertical: 4),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      elevation: 3,
+                      shadowColor: Colors.black,
+                      child: Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: ListTile(
+                            leading: Container(
+                              decoration: BoxDecoration(
+                                color: Color.fromARGB(
+                                    cores![0], cores[1], cores[2], cores[3]),
+                                borderRadius: BorderRadius.circular(100),
+                              ),
+                              child: CircleAvatar(
+                                backgroundColor: Colors.transparent,
+                                child: Text(
+                                  item.nome?.isNotEmpty == true
+                                      ? item.nome![0]
+                                      : '',
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            title: Text(
+                              "${item.nome} ${item.sobrenome}",
+                              style: const TextStyle(
+                                  fontSize: 17, fontWeight: FontWeight.w600),
+                            ),
+                            subtitle: Text(
+                              "${item.telefones?.celular}",
+                              style: const TextStyle(
+                                  fontSize: 15, fontWeight: FontWeight.w500),
+                            ),
+                          ))),
+                );
+              },
+            );
+          }
+        },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: () {},
+        child: const Icon(FontAwesomeIcons.plus),
+      ),
     );
   }
 }
